9d8f93dd8b9dc2326d306561a0eb8e521f86d082
wyvern8 <wyvern@smartcreations.com.au>
2018-03-17 10:20:50 +1100

    sonar debug

--- workspace/sonar.sh
+++ workspace/sonar.sh
@@ -4,11 +4,11 @@ cd /usr/workspace
 
 echo ">>> sonar scan.."
 if [[ -n $SONAR_SOURCES ]]; then
-    sonar-scanner -Dsonar.sources=$SONAR_SOURCES \
+    sonar-scanner -X -Dsonar.sources=$SONAR_SOURCES \
                     -Dsonar.projectKey=${SONAR_GITHUB_REPOSITORY/\//-} \
                     -Dsonar.projectName="$SONAR_PROJECTNAME_PREFIX${SONAR_GITHUB_REPOSITORY/\// :: }"
 else
-    sonar-scanner -Dsonar.sources=/usr/workspace/clone/src \
+    sonar-scanner -X -Dsonar.sources=/usr/workspace/clone/src \
                     -Dsonar.projectKey=${SONAR_GITHUB_REPOSITORY/\//-} \
                     -Dsonar.projectName="$SONAR_PROJECTNAME_PREFIX${SONAR_GITHUB_REPOSITORY/\// :: }"
 fi
