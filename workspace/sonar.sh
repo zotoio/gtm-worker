1795ebceb7c6aa3d56a5e68ac9ad1da25604189f
wyvern8 <wyvern@smartcreations.com.au>
2018-03-19 10:54:42 +1100

    sonar binaries

--- workspace/sonar.sh
+++ workspace/sonar.sh
@@ -5,7 +5,6 @@ cd /usr/workspace/clone
 echo ">>> sonar scan.."
 if [[ -n $SONAR_SOURCES ]]; then
     sonar-scanner -X -Dsonar.sources=$SONAR_SOURCES \
-                    -Dsonar.modules=${SONAR_MODULES} \
                     -Dsonar.java.binaries=${SONAR_JAVA_BINARIES} \
                     -Dsonar.projectKey=${SONAR_GITHUB_REPOSITORY/\//-} \
                     -Dsonar.projectName="$SONAR_PROJECTNAME_PREFIX${SONAR_GITHUB_REPOSITORY/\// :: }"
