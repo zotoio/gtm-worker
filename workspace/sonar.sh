0eb56427ebb2edd94c61e38447d160e89a56160d
wyvern8 <wyvern@smartcreations.com.au>
2018-03-21 17:07:47 +1100

    enterprise github sonar

--- workspace/sonar.sh
+++ workspace/sonar.sh
@@ -2,14 +2,18 @@
 
 cd /usr/workspace/clone
 
+if [[ -n $SONAR_MODULES ]]; then
+    SONAR_MODULES_PARAM=" -Dsonar.modules=$SONAR_MODULES "
+fi
+
 echo ">>> sonar scan.."
 if [[ -n $SONAR_SOURCES ]]; then
-    sonar-scanner -X -Dsonar.sources=$SONAR_SOURCES \
+    sonar-scanner -X -Dsonar.sources=$SONAR_SOURCES $SONAR_MODULES_PARAM \
                     -Dsonar.java.binaries=${SONAR_JAVA_BINARIES} \
                     -Dsonar.projectKey=${SONAR_GITHUB_REPOSITORY/\//-} \
                     -Dsonar.projectName="$SONAR_PROJECTNAME_PREFIX${SONAR_GITHUB_REPOSITORY/\// :: }"
 else
-    sonar-scanner -X -Dsonar.sources=src \
+    sonar-scanner -X -Dsonar.sources=src $SONAR_MODULES_PARAM \
                     -Dsonar.java.binaries=target \
                     -Dsonar.projectKey=${SONAR_GITHUB_REPOSITORY/\//-} \
                     -Dsonar.projectName="$SONAR_PROJECTNAME_PREFIX${SONAR_GITHUB_REPOSITORY/\// :: }"
