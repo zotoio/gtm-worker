31c96c29fa254123d6fa8557f5ce61b6f51ea202
AV <wyvern8@users.noreply.github.com>
2018-04-11 14:51:19 +1000

    Update build.sh

--- workspace/build.sh
+++ workspace/build.sh
@@ -6,6 +6,16 @@ if [ -n "$BUILD_COMMAND" ]; then
     echo ">>> running custom build commmand.."
     eval "$BUILD_COMMAND"
 else
+
+    if [[ -z "$BUILD_TYPE" ]]; then
+        if [[ -f 'pom.xml' ]]; then
+            BUILD_TYPE='maven'
+        fi
+        if [[ -f 'build.gradle' ]]; then
+            BUILD_TYPE='gradle'
+        fi
+    fi
+
     case "$BUILD_TYPE" in
             nodejs)
                 echo ">>> running node build.."
