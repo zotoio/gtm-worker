b8a903c84020778d646a9e141fd51d528534a6be
AV <wyvern8@users.noreply.github.com>
2018-04-11 14:50:20 +1000

    Update sonar.sh

--- workspace/sonar.sh
+++ workspace/sonar.sh
@@ -2,40 +2,46 @@
 
 cd /usr/workspace/clone
 
-if [[ ! $SONAR_KEEP_PROJECT_PROPERTIES = 'true' ]]; then
+if [[ -z "$SONAR_SOURCES" ]]; then
+    SONAR_SOURCES='src/main'
+fi
+
+if [[ -z "$SONAR_JAVA_BINARIES" ]]; then
+    if [[ -f 'pom.xml' ]]; then
+        SONAR_JAVA_BINARIES='target/classes'
+    fi
+    if [[ -f 'build.gradle' ]]; then
+        SONAR_JAVA_BINARIES='build/classes'
+    fi
+fi
+
+# if a bundle dir exists, assume aem structure
+if [[ -d 'bundle' ]]; then
+    if [[ ! $SONAR_MODULES ]]; then
+        SONAR_MODULES='bundle'
+    fi
+fi
+
+if [[ -z "$SONAR_EXCLUSIONS" ]]; then
+    SONAR_EXCLUSIONS='src/test/**/*'
+fi
+
+# if a root src dir exists, assume no modules subdirs
+
+if [[ ! "$SONAR_KEEP_PROJECT_PROPERTIES" = "true" ]]; then
     # Delete Sonar Properties as they Clash with the Pull Request Scan
     rm -f sonar-project.properties
-fi
-
-if [[ -n $SONAR_MODULES ]]; then
-    if [[ ! -d "$SONAR_MODULES" ]]; then
-        echo ">>> No Modules Directory. Exiting..."
-        exit 1;
-    fi
-    SONAR_MODULES_PARAM=" -Dsonar.modules=$SONAR_MODULES "
-fi
-
-# Test for Binaries and Sources Directories
-#if [[ ! -d "$SONAR_JAVA_BINARIES" ]]; then
-#    echo ">>> No Binaries Directory. Exiting..."
-#    exit 1;
-#el
-if [[ ! -d "$SONAR_MODULES/$SONAR_SOURCES" ]]; then
-    echo ">>> No Sources Directory. Exiting..."
-    exit 1;
+    echo sonar.sources=$SONAR_SOURCES >> sonar-project.properties
+    echo sonar.tests=$SONAR_TESTS >> sonar-project.properties
+    echo sonar.java.binaries=$SONAR_JAVA_BINARIES >> sonar-project.properties
+    echo sonar.exclusions=$SONAR_EXCLUSIONS >> sonar-project.properties
+    echo sonar.modules=$SONAR_MODULES >> sonar-project.properties
+    echo sonar.language=$SONAR_LANGUAGE >> sonar-project.properties
 fi
 
 echo ">>> sonar scan.."
-if [[ -n $SONAR_SOURCES ]]; then
-    sonar-scanner -X -Dsonar.sources=$SONAR_SOURCES $SONAR_MODULES_PARAM \
-                    -Dsonar.java.binaries=${SONAR_JAVA_BINARIES} \
-                    -Dsonar.projectKey=${SONAR_GITHUB_REPOSITORY/\//-} \
-                    -Dsonar.projectName="$SONAR_PROJECTNAME_PREFIX${SONAR_GITHUB_REPOSITORY/\// :: }"
-else
-    sonar-scanner -X -Dsonar.sources=src $SONAR_MODULES_PARAM \
-                    -Dsonar.java.binaries=target \
-                    -Dsonar.projectKey=${SONAR_GITHUB_REPOSITORY/\//-} \
-                    -Dsonar.projectName="$SONAR_PROJECTNAME_PREFIX${SONAR_GITHUB_REPOSITORY/\// :: }"
-fi
+sonar-scanner -X \
+    -Dsonar.projectKey=${SONAR_GITHUB_REPOSITORY/\//-} \
+    -Dsonar.projectName="$SONAR_PROJECTNAME_PREFIX${SONAR_GITHUB_REPOSITORY/\// :: }"
 
 cd /usr/workspace
